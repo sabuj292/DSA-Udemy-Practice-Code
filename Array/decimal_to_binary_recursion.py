18e4bf94bec3321739e9cf1edc01d8a6ea0f7889
sabuj292 <shahriarmahmud292@gmail.com>
2023-10-22 17:20:29 +0600

    decimal to binary

---
+++ Array/decimal_to_binary_recursion.py
@@ -0,0 +1,12 @@
+# converting decimal number to binary
+
+# Formula: f(n) = n mod 2 + 10 * f(n/2)
+
+def decimalToBinary(n):
+    # assert int(n) == n, 'The parameter must be an integer only'
+    if n == 0:
+        return 0
+    else:
+        return n%2 + 10 * decimalToBinary(int(n/2))
+
+print(decimalToBinary(15))
